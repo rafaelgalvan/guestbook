27d17ad254f7bad154e1a905167dd775fdd46bd8
Rafael Galvan <rafaelgalvan@outlook.com>
2020-12-12 11:20:59 -0300

    adding debug tools

--- public/index.php
+++ public/index.php
@@ -23,5 +23,6 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
+dump($request);
 $response->send();
 $kernel->terminate($request, $response);
